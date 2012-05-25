0660391248d0463b373920d497b3903fc669a802
Jonathan Brumley <cayasso@gmail.com>
2012-05-24 18:37:13 -0600

    Some changes

--- skip.js
+++ skip.js
@@ -1,142 +1,151 @@
-var vendorPrefixes = ['-webkit-', '-moz-', '-ms-', '-o-', ''];
+(function (window, document, undefined) {
 
-window.Skip = function (element, options) {
+  "use strict";
 
-  // set element
-  this.Skip = element;
+  var vendorPrefixes = ['-webkit-', '-moz-', '-ms-', '-o-', ''],
 
-  // determine width of each slide
-  this.width = this.Skip.getBoundingClientRect().width || this.Skip.offsetWidth;
-  this.height = this.Skip.getBoundingClientRect().height || this.Skip.offsetHeight;
+      Skip = function (element, options) {
 
-  // return immediately if measurement fails
-  if (!this.width) return;
+        // set element
+        this.Skip = element;
 
-  // is touch device?
-  var isTouch = this._isTouch  = ('ontouchstart' in document.documentElement);
-  
-  // Set event names
-  this._eventStart = isTouch ? 'touchstart' : 'mousedown';
-  this._eventEnd = isTouch ? 'touchend' : 'mouseup';
-  this._eventMove = isTouch ? 'touchmove' : 'mousemove';
+        // determine width of each slide
+        this.width = this.Skip.getBoundingClientRect().width || this.Skip.offsetWidth;
+        this.height = this.Skip.getBoundingClientRect().height || this.Skip.offsetHeight;
 
-  // x y positions
-  this._x = 0;
-  this._y = 0;
+        // return immediately if measurement fails
+        if (!this.width) return;
 
-  // start positions
-  this._startX = 0;
-  this._startY = 0;
+        // is touch device?
+        var isTouch = this._isTouch  = ('ontouchstart' in document.documentElement);
+        
+        // Set event names
+        this._eventStart = isTouch ? 'touchstart' : 'mousedown';
+        this._eventEnd = isTouch ? 'touchend' : 'mouseup';
+        this._eventMove = isTouch ? 'touchmove' : 'mousemove';
 
-  // axis directions
-  this._dirX = 'left';
-  this._dirY = 'up';
+        // x y positions
+        this._x = 0;
+        this._y = 0;
 
-  this.margin = 250;
-  this._margin = 250;
+        // start positions
+        this._startX = 0;
+        this._startY = 0;
 
-  // trigger initialization
-  this.setup();
+        // axis directions
+        this._dirX = 'left';
+        this._dirY = 'up';
 
-  // bind events
-  this.on();
-  
-};
+        this.margin = 250;
+        this._margin = 250;
 
-Skip.prototype.setup = function () {
-  
-};
+        // trigger initialization
+        this.setup();
 
-Skip.prototype.on = function (e) {
-  // add event listeners
-  if (this.Skip.addEventListener) {
-    this.Skip.addEventListener(this._eventStart, this, false);
-    this.Skip.addEventListener(this._eventMove, this, false);
-    this.Skip.addEventListener(this._eventEnd, this, false);
-    window.addEventListener('resize', this, false);
-  }
-};
+        // bind events
+        this.on();
+        
+      };
 
-Skip.prototype.off = function (e) {
-  // remove event listeners
-  if (this.Skip.addEventListener) {
-    this.Skip.removeEventListener(this._eventStart, this, false);
-    this.Skip.removeEventListener(this._eventMove, this, false);
-    this.Skip.removeEventListener(this._eventEnd, this, false);
-    window.removeEventListener('resize', this, false);
-  }
-};
-
-// Handle events
-Skip.prototype.handleEvent = function(e) {
-  switch(e.type) {
-    case this._eventStart: this.onTouchStart(e); break;
-    case this._eventMove: this.onTouchMove(e); break;
-    case this._eventEnd: this.onTouchEnd(e); break;
-  }
-};
-
-Skip.prototype.onTouchStart = function (e) {
-  this._start = {
-    // get touch coordinates for delta calculations in onTouchMove
-    pageX: e.touches[0].pageX,
-    pageY: e.touches[0].pageY,
-
-    // set initial timestamp of touch sequence
-    time: Number( new Date() )
+  Skip.prototype.setup = function () {
+    
   };
-};
 
-Skip.prototype.onTouchMove = function (e) {
+  Skip.prototype.on = function (e) {
+    // add event listeners
+    if (this.Skip.addEventListener) {
+      this.Skip.addEventListener(this._eventStart, this, false);
+      this.Skip.addEventListener(this._eventMove, this, false);
+      this.Skip.addEventListener(this._eventEnd, this, false);
+      window.addEventListener('resize', this, false);
+    }
+  };
 
-  // ensure swiping with one touch and not pinching
-  if(this._isTouch && e.touches.length > 1 || e.scale && e.scale !== 1) return;
+  Skip.prototype.off = function (e) {
+    // remove event listeners
+    if (this.Skip.addEventListener) {
+      this.Skip.removeEventListener(this._eventStart, this, false);
+      this.Skip.removeEventListener(this._eventMove, this, false);
+      this.Skip.removeEventListener(this._eventEnd, this, false);
+      window.removeEventListener('resize', this, false);
+    }
+  };
 
-  var myX = (this._isTouch ? e.touches[0] : e).pageX;
-  var myY = (this._isTouch ? e.touches[0] : e).pageY;
+  // Handle events
+  Skip.prototype.handleEvent = function(e) {
+    switch(e.type) {
+      case this._eventStart: this.onTouchStart(e); break;
+      case this._eventMove: this.onTouchMove(e); break;
+      case this._eventEnd: this.onTouchEnd(e); break;
+    }
+  };
 
-  this._dirX = (myX > this._x) ? 'right' : 'left';
-  //this._dirY = (myY > this._y) ? 'down' : 'up';
+  Skip.prototype.onTouchStart = function (e) {
+    this._start = {
+      // get touch coordinates for delta calculations in onTouchMove
+      pageX: e.touches[0].pageX,
+      pageY: e.touches[0].pageY,
 
-  this._x = myX;
-  //this._y = myY;
+      // set initial timestamp of touch sequence
+      time: Number( new Date() )
+    };
+  };
 
-  if (this._dirX === 'left') {
+  Skip.prototype.onTouchMove = function (e) {
 
-    if (this.margin < 0)
-      this.margin = 0;
-    else
-      this.margin = this.margin - 10;
+    // ensure swiping with one touch and not pinching
+    if(this._isTouch && e.touches.length > 1 || e.scale && e.scale !== 1) return;
 
-    $('.content').css('-webkit-transform', 'translate3d('+ this.margin +'px, 0px, 0px)');
+    var myX = (this._isTouch ? e.touches[0] : e).pageX;
+    var myY = (this._isTouch ? e.touches[0] : e).pageY;
 
-  } else {
-    if (this.margin > 280)
-      this.margin = 250;
-    else
-      this.margin = this.margin + 10;
+    this._dirX = (myX > this._x) ? 'right' : 'left';
+    //this._dirY = (myY > this._y) ? 'down' : 'up';
 
-    $('.content').css('-webkit-transform', 'translate3d('+ this.margin +'px, 0px, 0px)');
-  }
-};
+    this._x = myX;
+    //this._y = myY;
 
-Skip.prototype.onTouchEnd = function (e) {
+    if (this._dirX === 'left') {
 
-};
+      if (this.margin < 0)
+        this.margin = 0;
+      else
+        this.margin = this.margin - 10;
 
-Skip.prototype.onTransitionEnd = function () {
+      $('.content').css('-webkit-transform', 'translate3d('+ this.margin +'px, 0px, 0px)');
 
-};
+    } else {
+      if (this.margin > 280)
+        this.margin = 250;
+      else
+        this.margin = this.margin + 10;
 
-Skip.prototype._move = function () {
+      $('.content').css('-webkit-transform', 'translate3d('+ this.margin +'px, 0px, 0px)');
+    }
+  };
 
-};
+  Skip.prototype.onTouchEnd = function (e) {
 
-Skip.prototype._animate = function () {
+  };
 
-};
+  Skip.prototype.onTransitionEnd = function () {
 
-Skip.prototype._translate = function () {
+  };
 
-};
+  Skip.prototype._move = function () {
+
+  };
+
+  Skip.prototype._animate = function () {
+
+  };
+
+  Skip.prototype._translate = function () {
+
+  };
+
+  return Skip;
+
+
+}).call(this, window, document);
 
